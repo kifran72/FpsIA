26c3e615c07ba8e2af24bb7c48f2ded64b1d976e
kifran <rizzon.charles@gmail.com>
2021-05-02 00:32:55 +0200

    Fix home page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,9 @@ import { UserRoute } from './user';
 
 export const routes = (app: any, twing: any) => {
     app.get('/', function(req: any, res: any) {
-        if (!req.session.isConnected) {
-            res.redirect('/login');
-        } else {
-            twing.render("index.twig", { name: req.session.name }).then((output: any) => {
-                res.end(output);
-            });
-        }
+        twing.render("index.twig", { name: req.session.name }).then((output: any) => {
+            res.end(output);
+        });
     });
 
     UserRoute(app, twing);
