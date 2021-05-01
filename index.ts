e8ad3927878578fe734bc54b543399cfd35396c9
kifran <rizzon.charles@gmail.com>
2021-05-02 00:11:56 +0200

    save

--- index.ts
+++ index.ts
@@ -27,40 +27,41 @@ app.use(session({
 dotenv.config({ path: 'key.env' });
 
 const password = process.env.PASSWORD;
-ssh.connect({
-    host: '81.185.106.245',
-    username: 'iafps',
-    port: 900,
-    privateKey: './id_rsa',
-    password
-}).then((resp: any) => {
-    if (resp) {
-        // let videoName = 'video.mp4';
-        // ssh.putFiles([{ local: './' + videoName, remote: '/ia' }]).then(function() {
-        //     console.log("The File thing is done")
-        // }, function(error: any) {
-        //     console.log("Something's wrong")
-        //     console.log(error)
-        // });
-        // ssh.exec('python3 inference_video.py', ['--exp=1', '--video=' + videoName], {
-        //     cwd: '/ia',
-        //     onStdout(chunk: any) {
-        //         console.log('stdoutChunk', chunk.toString('utf8'))
-        //     },
-        //     onStderr(chunk: any) {
-        //         console.log('stderrChunk', chunk.toString('utf8'))
-        //     },
-        // });
 
-        // ssh.getFile('./' + videoName, '/ia').then(function(Contents: any) {
-        //     console.log("The File's contents were successfully downloaded")
-        // }, function(error: any) {
-        //     console.log("Something's wrong")
-        //     console.log(error)
-        // });
-        console.log('ça marche');
-    }
-})
+// ssh.connect({
+//     host: '81.185.106.245',
+//     username: 'iafps',
+//     port: 900,
+//     privateKey: './id_rsa',
+//     password
+// }).then((resp: any) => {
+//     if (resp) {
+//         let videoName = 'video.mp4';
+//         ssh.putFiles([{ local: './' + videoName, remote: '/ia' }]).then(function() {
+//             console.log("The File thing is done")
+//         }, function(error: any) {
+//             console.log("Something's wrong")
+//             console.log(error)
+//         });
+//         ssh.exec('python3 inference_video.py', ['--exp=1', '--video=' + videoName], {
+//             cwd: '/ia',
+//             onStdout(chunk: any) {
+//                 console.log('stdoutChunk', chunk.toString('utf8'))
+//             },
+//             onStderr(chunk: any) {
+//                 console.log('stderrChunk', chunk.toString('utf8'))
+//             },
+//         });
+
+//         ssh.getFile('./' + videoName, '/ia').then(function(Contents: any) {
+//             console.log("The File's contents were successfully downloaded")
+//         }, function(error: any) {
+//             console.log("Something's wrong")
+//             console.log(error)
+//         });
+//         console.log('ça marche');
+//     }
+// })
 
 
 
